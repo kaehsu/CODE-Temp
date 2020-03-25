9f137197a2539e5829507c6f1c50f25142e7d8cf
Kae Hsu <kaehsu@gmail.com>
2020-03-25 11:17:30 +0800

    Program initial

---
+++ signalsend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SIGN=$1
+SSPID=$2
+
+declare -a BYPASS=(2 9 19 32 33)
+
+while [ $SIGN -le 64 ]; do
+  [[ ! " ${BYPASS[@]} " =~ " ${SIGN} " ]] && echo "Send signal $SIGN" && kill -$SIGN $SSPID
+  SIGN=$((SIGN +1))
+  sleep 1
+done
