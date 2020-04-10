83d16b96566bab5d7e198f8377870f2e2315551a
Kae Hsu <kaehsu@gmail.com>
2020-04-10 10:03:19 +0800

    File initial

---
+++ dialog/dl_msgbox2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DIALOG="/usr/bin/dialog"
+
+$DIALOG --msgbox "Dialog msgbox test" 10 40
+
+if [ $? -eq 0 ]; then
+	echo 'Enter key detected'
+elif [ $? -eq 255 ]; then
+	echo 'ESC key detected'
+else
+	echo 'Ctrl-C detected'
+fi
