2acfd290bc7b5c4c5bb969f1205daa18e7478634
Kae Hsu <kaehsu@gmail.com>
2020-08-04 10:56:25 +0800

    Program update

--- Python/panel/portex_led.py
+++ Python/panel/portex_led.py
@@ -3,13 +3,14 @@
 import sys
 import socket
 import json
+import RPi.GPIO as GPIO
 
 # Read configuration from config files
 ledCONFIGfile = 'portex_led.conf'
 with open(ledCONFIGfile, 'r') as file:
     ledCONFIG = file.read()
 
-dictStatus = {
+ledStatustemp = {
     "status": {
         "opMode": {
             "freq": 0,
@@ -22,10 +23,15 @@ dictStatus = {
     }
 }
 
+# Build necessary data structure
 ledCONFIG = json.loads(ledCONFIG)
-allLED = ledCONFIG.keys()
+allLED = ledCONFIG['allLED']['allLED'].split(',')
 for item in allLED:
-    ledCONFIG[item].update(dictStatus)
+    ledCONFIG[item].update(ledStatustemp)
     ledCONFIG[item]['status']['opMode'].update(ledCONFIG[item]['default'])
 
-statusFlag = {'currentStatus': 'opMode', 'lastStatus': 'opMode'}
+# GPIO initialization
+GPIO.setmode(GPIO.BCM)
+for item in allLED:
+    GPIO.setup(ledCONFIG[item]['gpioNum'], GPIO.OUT, GPIO.PUD_OFF, GPIO.LOW)
+    str(item)+'p' = GPIO.PWM(ledCONFIG[item]['gpioNum'], 1)
