57006f933dd5c35ae540fcc63b3cd3d6ce28abe6
Kae Hsu <kaehsu@gmail.com>
2020-10-19 18:04:49 +0800

    Code optimization

--- SenseHat/clear.py
+++ SenseHat/clear.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 
 from sense_hat import SenseHat
-from random import randint
 
 sense = SenseHat()
 
-r = 0
-g = 0
-b = 0
-
-print("R, G, B values are", r, g ,b)
-
-sense.clear((r, g, b))
+sense.clear()
--- SenseHat/showcolor.py
+++ SenseHat/showcolor.py
@@ -5,10 +5,11 @@ from random import randint
 
 sense = SenseHat()
 
-r = randint(0,255)
-g = randint(0,255)
-b = randint(0,255)
+r = randint(0,randint(1,255))
+g = randint(0,randint(1,255))
+b = randint(0,randint(1,255))
 
 print("R, G, B values are", r, g ,b)
 
 sense.clear((r, g, b))
+
