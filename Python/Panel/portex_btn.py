eb99fdeb7e3d8c03bbd9404e2def0f4563d3a899
Kae Hsu <kaehsu@gmail.com>
2020-12-08 18:01:01 +0800

    update

--- Python/Panel/portex_btn.py
+++ Python/Panel/portex_btn.py
@@ -2,36 +2,157 @@
 
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
+import os
+from random import randint
+import threading
+import signal
+import argparse
+from portex_exec import execCMD
 
-pBTNp = 22
-aBTNp = 27
-bBTNp = 17
-listBTN = [pBTNp, aBTNp, bBTNp]
+#
+# Command line argument
+parser = argparse.ArgumentParser(
+    description='PORTEX Panel Program for Panel Button')
+parser.add_argument('-b', '--bouncetime', type=int,
+                    help='GPIO bouncetime in milliseconds, range 1ms to 512ms, default 64ms', default=64)
+parser.add_argument('-t', '--timeout', type=int,
+                    help='Button press timeout in seconds, range 2s to 16s, default 6s', default=6)
+cargs = parser.parse_args()
 
-def event_occurredP(btn):
-    return 'Button ' + str(btn) + ' is pressed.'
+#
+# Input GPIO bouncetime check
+# By serial testing, GPIO bouncetime should less than 512ms
+if 1 <= cargs.bouncetime <= 512:
+    gpioBouncetime = cargs.bouncetime
+    # print('Button GPIO bouncetime is {}ms.'.format(gpioBouncetime))
+else:
+    # print('Out of suggestted bouncetime range, change bouncetime to default {} ms.'.format(64))
+    gpioBouncetime = 64
 
-def event_occurredA(btn):
-    return 'Button ' + str(btn) + ' is pressed.'
+#
+# Input button timeout check, maximun timeout is set to 16 seconds
+# Default is 6 seconds,
+if 2 <= cargs.timeout <= 16:
+    cbtnTimeout = cargs.timeout
+    # print('Button timeout is {} second(S)'.format(cbtnTimeout))
+else:
+    cbtnTimeout = 6
+    # print('Out of suggestted timeout range, change timeout to default {} seconds.'.format(cbtnTimeout))
 
-def event_occurredB(btn):
-    return 'Button ' + str(btn) + ' is pressed.'
+
+#
+# Define GPIO# and mapping to the button name
+btnMapping = {'22': 'btnP', '27': 'btnA', '17': 'btnB'}
+
+#
+# Maintain button status
+# Use dictionary
+statP, statA, statB = [], [], []
+btnStatus = {'btnP': statP, 'btnA': statA, 'btnB': statB}
+
+#
+# Button press period list initial
+listP, listA, listB = [], [], []
+periodList = {'btnP': listP, 'btnA': listA, 'btnB': listB}
 
 
+#
+# GPIO button initial
 GPIO.setmode(GPIO.BCM)
-for item in listBTN:
-    GPIO.setup(item, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+for item in btnMapping.keys():
+    GPIO.setup(int(item), GPIO.IN)
 
-GPIO.add_event_detect(pBTNp, GPIO.BOTH, callback=event_occurredP)
-GPIO.add_event_detect(aBTNp, GPIO.BOTH, callback=event_occurredA)
-GPIO.add_event_detect(bBTNp, GPIO.BOTH, callback=event_occurredB)
+
+def exportResult(btnName, pressPeriodSec, pressPeriodmSec):
+    '''
+    Export button action result.
+    '''
+    print("Button {} press interval is {:03d}.{:06d} seconds\n".format(
+        btnName, pressPeriodSec, pressPeriodmSec))
+
+
+def countDown(btnName, btnTimeout, btnSessionID):
+    '''
+    Each time a button is pressed, a timer will start to countdown for button timeout.
+    '''
+    while btnTimeout > 0:
+        time.sleep(1)
+        btnTimeout -= 1
+    if len(btnStatus.get(btnName)) > 0 and btnSessionID == btnStatus.get(btnName)[1]:
+        # print("Button {} timeout, session ID: {}".format(btnName, btnSessionID))
+        btnStatus.get(btnName).append('reset')
+        # print(btnStatus.get(btnName), len(btnStatus.get(btnName)))
+        # exportResult(btnName, cbtnTimeout, 0)
+        execCMD(btnName, cbtnTimeout)
+
+
+def periodCalc(pin):
+    '''
+    Calculate time period between two button actions.
+    '''
+    btnName = btnMapping.get(str(pin))
+    btnTime = datetime.now()
+    # print("The length of {} before appending is {}".format(btnName, len(periodList.get(btnName))))
+    periodList.get(btnName).append(btnTime)
+    # print("The length of {} after appending is {}".format(btnName, len(periodList.get(btnName))))
+    #
+    # Check if the button is reseted
+    if len(btnStatus.get(btnName)) == 3:
+        # print("Ignore button {} action this time.\n".format(btnName))
+        periodList.get(btnName).clear()
+        btnStatus.get(btnName).clear()
+    #
+    #
+    # If the button is pressed & released, calculate the period
+    elif len(periodList.get(btnName)) == 2:
+        diffSec = (periodList.get(btnName)[
+            1] - periodList.get(btnName)[0]).seconds
+        diffmSec = (periodList.get(btnName)[
+            1] - periodList.get(btnName)[0]).microseconds
+        # exportResult(btnName, diffSec, diffmSec)
+        execCMD(btnName, diffSec+diffmSec/1000000)
+        periodList.get(btnName).clear()
+        btnStatus.get(btnName).clear()
+    else:
+        btnStatus.get(btnName).append(1)
+        btnSessionID = randint(65000, 65535)
+        btnStatus.get(btnName).append(btnSessionID)
+        #
+        #
+        # Start to button pressed countdown
+        p = threading.Thread(target=countDown, args=(
+            btnName, cbtnTimeout, btnSessionID,))
+        p.start()
+        # print("Button {}, id {} is pressed and waiting for release.....".format(btnName, btnStatus.get(btnName)[1]))
+
+
+def hangUP(signum, frame):
+    GPIO.cleanup()
+    exit()
+
+
+def kbInterrupt(signum, frame):
+    GPIO.cleanup()
+    raise KeyboardInterrupt()
+
+
+#
+# By real evaluating, long bouncetime cause button action not being detected easy (extreming condition)
+for item in btnMapping.keys():
+    GPIO.add_event_detect(int(item), GPIO.BOTH,
+                          callback=periodCalc, bouncetime=gpioBouncetime)
+
+signal.signal(signal.SIGHUP, hangUP)
+signal.signal(signal.SIGINT, kbInterrupt)
 
 try:
     while True:
-        print('Waiting button to be pressed.')
-        time.sleep(86400)
+        # print('Waiting button to be pressed.....')
+        # time.sleep(32)
+        # print('Clear monitor after 2 seconds')
+        # time.sleep(2)
+        # os.system("clear")
+        pass
 except KeyboardInterrupt:
-    print("Program is interrupted.")
-
-GPIO.cleanup()
-
+    pass
--- Python/Panel/portex_exec.py
+++ Python/Panel/portex_exec.py
@@ -4,33 +4,33 @@ import yaml
 import subprocess
 import argparse
 
-with open('portex_run.yaml', 'r') as configFILE:
+with open('portex_cmd.yaml', 'r') as configFILE:
     CONFIG = configFILE.read()
 runCONFIG = yaml.safe_load(CONFIG)
 
 
-def execPROG(btnX, time):
+def execCMD(btnX, time):
     pressBTN = btnX
     pressTIME = time
     if pressBTN not in ('btnA', 'btnB', 'btnP'):
         print("Check button name: button name should be btnP, btnA or btnB")
         exit(1)
     if runCONFIG['btnPeriod']['prdA'] < pressTIME and pressTIME <= runCONFIG['btnPeriod']['prdB']:
-        # print("Button {} exec {}".format(pressBTN, runCONFIG[pressBTN + '_A']['run']))
+        # print("Button {} exec {}".format(pressBTN, runCONFIG[pressBTN + '_A']['cmd']))
         processRET = subprocess.call(
-            runCONFIG[pressBTN + '_A']['run'], shell=True)
+            runCONFIG[pressBTN + '_A']['cmd'], shell=True)
     elif runCONFIG['btnPeriod']['prdB'] < pressTIME and pressTIME <= runCONFIG['btnPeriod']['prdC']:
-        # print("Button {} exec {}".format(pressBTN, runCONFIG[pressBTN + '_B']['run']))
+        # print("Button {} exec {}".format(pressBTN, runCONFIG[pressBTN + '_B']['cmd']))
         processRET = subprocess.call(
-            runCONFIG[pressBTN + '_B']['run'], shell=True)
+            runCONFIG[pressBTN + '_B']['cmd'], shell=True)
     elif runCONFIG['btnPeriod']['prdC'] < pressTIME and pressTIME <= runCONFIG['btnPeriod']['prdD']:
-        # print("Button {} exec {}".format(pressBTN, runCONFIG[pressBTN + '_C']['run']))
+        # print("Button {} exec {}".format(pressBTN, runCONFIG[pressBTN + '_C']['cmd']))
         processRET = subprocess.call(
-            runCONFIG[pressBTN + '_C']['run'], shell=True)
+            runCONFIG[pressBTN + '_C']['cmd'], shell=True)
     elif runCONFIG['btnPeriod']['prdD'] < pressTIME:
-        # print("Button {} exec {}".format(pressBTN, runCONFIG[pressBTN + '_D']['run']))
+        # print("Button {} exec {}".format(pressBTN, runCONFIG[pressBTN + '_D']['cmd']))
         processRET = subprocess.call(
-            runCONFIG[pressBTN + '_D']['run'], shell=True)
+            runCONFIG[pressBTN + '_D']['cmd'], shell=True)
     else:
         print('No matched button name.')
 
@@ -39,13 +39,15 @@ def main():
     #
     # Command line argument
     parser = argparse.ArgumentParser(
-        description='PORTEX Panel Program to execute pre-defined program')
-    parser.add_argument('-b', '--button', type=str, help='Pressed button')
-    parser.add_argument('-p', '--period', type=str, help='Press period')
+        description='PORTEX Panel Program to execute pre-defined command.')
+    parser.add_argument('-b', '--button', type=str,
+                        help='Pressed button', required=True)
+    parser.add_argument('-p', '--period', type=str,
+                        help='Press period', required=True)
     cargs = parser.parse_args()
     pressButton = cargs.button
     pressPeriod = cargs.period
-    execPROG(pressButton, float(pressPeriod))
+    execCMD(pressButton, float(pressPeriod))
 
 
 if __name__ == "__main__":
